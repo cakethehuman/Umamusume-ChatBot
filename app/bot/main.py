48af39f1d3ab13211868f43a04aa3a58bb707b66
cakethehuman <willsonputra17@gmail.com>
2026-08-09 20:05:30 +0700

    added basic hello command

--- app/bot/main.py
+++ app/bot/main.py
@@ -1,11 +1,18 @@
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.ext import ContextTypes, Application, CommandHandler
 
-from ..core.config import settings
+from app.core.config import settings
 
 async def hello(update: Update, content: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Hello this bot was made by cake")
     
-# def main() -> None:
-#     application =APp
+def main() -> None:
+    application = Application.builder().token(settings.BOT_TOKEN).build()
+    
+    application.add_handler(CommandHandler("Hello", hello))
+    
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+    
+if __name__ == "__main__":
+    main()
     
