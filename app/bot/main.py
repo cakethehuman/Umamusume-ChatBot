01de188427dd9c7edbae6d8c30028c2274c89d72
cakethehuman <willsonputra17@gmail.com>
2026-08-10 13:15:57 +0700

    added logger

--- app/bot/main.py
+++ app/bot/main.py
@@ -1,9 +1,17 @@
+import logging
+
 from telegram import Update
 from telegram.ext import ContextTypes, Application, CommandHandler
 
+from app.core.logger import setup_logging
 from app.core.config import settings
 
+setup_logging()
+
+logger = logging.getLogger(__name__)
+
 async def hello(update: Update, content: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info("Used hello comand")
     await update.message.reply_text("Hello this bot was made by cake")
     
 def main() -> None:
@@ -14,5 +22,6 @@ def main() -> None:
     application.run_polling(allowed_updates=Update.ALL_TYPES)
     
 if __name__ == "__main__":
+    logger.info("Bot has turned on")
     main()
     
