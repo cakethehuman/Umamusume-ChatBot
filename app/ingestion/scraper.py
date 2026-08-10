49d2428af47f698372249f9fd79868d90dff1977
cakethehuman <willsonputra17@gmail.com>
2026-08-11 01:36:23 +0700

    Made the scraper better and give direction where the project scraper is going to go

--- app/ingestion/scraper.py
+++ app/ingestion/scraper.py
@@ -12,21 +12,31 @@ logger = logging.getLogger(__name__)
 CHARACTERS = ["Rice_Shower"]
 BASEURL = "https://umamusu.wiki/"
 
-def scrape_data(url, characters):
+def scrape_data(base_url, characters):
     for character in characters:
-        url = f"{url}{character}"
+        url = f"{base_url}{character}"
         logging.info(f"Fetching url for this {character} umamusume")
         
         reponse = requests.get(url)
         logging.info(reponse.status_code)
         if reponse.status_code == 200:
-            logging.info("Scraped succes")
-            print("it work bro")
+            logging.info("Scraped status code 200")
+            soup = BeautifulSoup(reponse.text, 'html.parser')
+            data = []
+            
+            tag_find = soup.find_all(['h1','h2','p','tr','span'])
+            for tag in tag_find:
+                uma_data = tag.text.strip()
+                data.append(uma_data)
+            return {
+                "title" : character,
+                "url" : url,
+                "content" : " ".join(data).strip()
+            }
+     
         else:
-            logging.info("Cant scrape")
-            print("It did not work bro")
+            logging.info(f"Failed to scrape status code : {reponse.status_code}")
+
         
-        
-    
-scrape_data(url=BASEURL,
-            characters=CHARACTERS)
+data_result = scrape_data(base_url=BASEURL, characters=CHARACTERS)
+print(data_result)
